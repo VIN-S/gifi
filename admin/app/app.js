e2ef526b00c1738e3e1c419a36e9271437c54260
vin <syx0628@gmail.com>
2017-02-18 15:36:10 +0800

    sign in redirect

--- admin/app/app.js
+++ admin/app/app.js
@@ -1,12 +1,20 @@
 //Define an angular module for our app
 var app = angular.module('myApp', []);
  
-app.controller('gifiAdminController', function($scope, $http) {
+app.controller('gifiAdminController', function($scope, $http, $window) {
   $scope.adminSignIn = function(username, pwd) {
     $http.post("ajax/adminSignIn.php?username="+username+"&pwd="+pwd)
     .then(function(response) {
         $scope.signinStatus = response.data.status;
         $scope.signinMessage = response.data.message;
+        if($scope.signinStatus === 'success'){
+        	if(alert($scope.signinMessage)){
+        		$window.location.href = 'partials/dashboard.html';
+        	}
+        }else{
+        	$scope.failInfo = 'Incorrect username or password';
+        	alert($scope.signinMessage);
+        }
     });
   };
 });
