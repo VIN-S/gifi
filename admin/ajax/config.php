9d75b1ae7fed3f3b868f4517eb886181f9891876
vin <syx0628@gmail.com>
2017-02-18 21:42:48 +0800

    finish upload csv

---
+++ admin/ajax/config.php
@@ -0,0 +1,11 @@
+<?php
+
+$host = "127.0.0.1";    /* Host name */
+$user = "root";         /* User */
+$password = "password";         /* Password */
+$dbname = "gifi";   /* Database name */
+
+$con = mysql_connect($host, $user, $password) or die("Unable to connect");
+
+// selecting database
+$db = mysql_select_db($dbname, $con) or die("Database not found");
--- admin/app/app.js
+++ admin/app/app.js
@@ -1,20 +1,54 @@
 //Define an angular module for our app
 var app = angular.module('myApp', []);
  
-app.controller('gifiAdminController', function($scope, $http, $window) {
+app.controller('gifiAdminController', function($scope, $http, $window, fileUpload) {
   $scope.adminSignIn = function(username, pwd) {
     $http.post("ajax/adminSignIn.php?username="+username+"&pwd="+pwd)
     .then(function(response) {
         $scope.signinStatus = response.data.status;
         $scope.signinMessage = response.data.message;
         if($scope.signinStatus === 'success'){
-        	if(alert($scope.signinMessage)){
-        		$window.location.href = 'partials/dashboard.html';
-        	}
+        	$window.location.href = 'partials/dashboard.html';
         }else{
         	$scope.failInfo = 'Incorrect username or password';
         	alert($scope.signinMessage);
         }
     });
   };
-});
+
+  $scope.uploadFile = function(){
+        var file = $scope.myFile;
+        console.log('file is ' );
+        console.dir(file);
+
+        var uploadUrl = "../ajax/upload.php";
+        fileUpload.uploadFileToUrl(file, uploadUrl);
+   };
+})
+
+.directive('fileModel', ['$parse', function ($parse) {
+    return {
+    restrict: 'A',
+    link: function(scope, element, attrs) {
+        var model = $parse(attrs.fileModel);
+        var modelSetter = model.assign;
+
+        element.bind('change', function(){
+            scope.$apply(function(){
+                modelSetter(scope, element[0].files[0]);
+            });
+        });
+    }
+   };
+}])
+
+// We can write our own fileUpload service to reuse it in the controller
+.service('fileUpload', ['$http', function ($http) {
+    this.uploadFileToUrl = function(file, uploadUrl){
+         $http.post(uploadUrl, file)
+         .then(function(response){
+         	console.log(response);
+            console.log("Success");
+         })
+     }
+ }]);
