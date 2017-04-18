b2a522ffb05c979addb2d95bc56d159a83f6625b
vin <syx0628@gmail.com>
2017-04-18 19:02:38 +0800

    finish compare with another country function

--- app/detailCtrl.js
+++ app/detailCtrl.js
@@ -2,9 +2,21 @@
 app.controller("detailCtrl", ['$http', '$scope', '$routeParams', function ($http, $scope, $routeParams) {
   var country = $routeParams.countryName;
   var year = $routeParams.selectedYear;
+  $scope.comparedCountry = "None"
 
   getCountryDetail();
   getRegionDetail();
+
+  //Get list of countries
+  getListOfCountries();
+
+  function getListOfCountries() {
+    $http.post("ajax/getListOfCountries.php")
+    .then(function(response) {
+        var temp = response.data.substring(1, response.data.length-1);
+        $scope.countryLists = temp.split("\"\"");
+    });
+  };
 
   function getCountryDetail(){
     $http.post("ajax/getCountryRanking.php?country="+country+"&year="+year)
@@ -76,6 +88,43 @@ app.controller("detailCtrl", ['$http', '$scope', '$routeParams', function ($http
           $scope.region.ease_of_doing_business = parseInt(results['ease_of_doing_business']);
           $scope.region.political_environment = parseInt(results['political_environment']);
           $scope.region.accounting_system = parseInt(results['accounting_system']);
+        }
+    });
+  };
+
+  $scope.selectComparedCountry = function(comparedCountry){
+    $http.post("ajax/getCountryRanking.php?country="+comparedCountry+"&year="+year)
+    .then(function(response) {
+        var results = response.data;
+        $scope.compared = [];
+
+        if (typeof results === 'undefined' || results === null || results === ""){
+          $scope.compared.countryName =  "";
+          $scope.compared.investor_friendliness_rank = "No Record";
+          $scope.compared.legal_and_regulatory_environment = "N.A.";
+          $scope.compared.market_development = "N.A.";
+          $scope.compared.exchange_controls_and_capital_restriction = "N.A.";
+          $scope.compared.corporate_governance = "N.A.";
+          $scope.compared.aum_levels_and_growth = "N.A.";
+          $scope.compared.banking_system = "N.A.";
+          $scope.compared.ease_of_doing_business = "N.A.";
+          $scope.compared.political_environment = "N.A.";
+          $scope.compared.accounting_system = "N.A.";
+        }else{
+          $scope.compared.countryName =  country;
+          if(results['investor_friendliness_rank'] == 0) 
+            $scope.compared.investor_friendliness_rank = ">100";
+          else
+            $scope.compared.investor_friendliness_rank = results['investor_friendliness_rank'];
+          $scope.compared.legal_and_regulatory_environment = results['legal_and_regulatory_environment'];
+          $scope.compared.market_development = results['market_development'];
+          $scope.compared.exchange_controls_and_capital_restriction = results['exchange_controls_and_capital_restriction'];
+          $scope.compared.corporate_governance = results['corporate_governance'];
+          $scope.compared.aum_levels_and_growth = results['aum_levels_and_growth'];
+          $scope.compared.banking_system = results['banking_system'];
+          $scope.compared.ease_of_doing_business = results['ease_of_doing_business'];
+          $scope.compared.political_environment = results['political_environment'];
+          $scope.compared.accounting_system = results['accounting_system'];
         }
     });
   }
