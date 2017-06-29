f529ed07c146ff7aa42fbd66b53024a51cf1c813
vin <syx0628@gmail.com>
2017-06-29 15:54:33 +0800

    upload ranking csv update

--- admin/ajax/upload_csv.php
+++ admin/ajax/upload_csv.php
@@ -71,7 +71,7 @@ if($error!=1)
 <!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"> 
+<link rel="stylesheet" href="../../css/bootstrap.min.css"> 
 </head>
 <body>
  
